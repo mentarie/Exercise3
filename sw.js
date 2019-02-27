9fe714cf8868e4238e6b119304dbe92195e2519d
Mentari E. Rizki <mentari.e.r@mail.ugm.ac.id>
2019-02-27 19:05:27 +0700

    push kedua
    fixing

--- sw.js
+++ sw.js
@@ -14,19 +14,13 @@ function fetchPerson(id){
       const name = json.name;
       const birth_year = json.birth_year;
 
-      /// If you were doing document.createElement
-      //const character = document.createElement('div');
-      //character.className = "character";
-      //character.textContent = name + ": " + birth_year;
-      //document.querySelector("#characters").appendChild(character)
-
-      const html =
-        '<div class="character">'
-          '<div class="name">'
-            '<a href="${json.url}">${name}</a>'
-          '</div>'
-          '<div class="year">${birth_year}</div>'
-        '</div>'
+      const html = `
+        <div class="character">
+          <div class="name">
+            <a href="${json.url}">${name}</a>
+          </div>
+          <div class="year">${birth_year}</div>
+        </div>
       
       document.querySelector("#characters").insertAdjacentHTML('afterbegin', html)
     })
